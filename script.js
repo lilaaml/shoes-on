b18ded78706a62e36b24b4972816a26952f67480
Lila Amelia <lilaamelia77@gmail.com>
2024-06-08 21:10:17 +0700

    product details almost finished

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var menuItems = document.getElementById('menuItems');
-
 menuItems.style.opacity = '100%';
 
 function menuToggle() {
@@ -8,4 +7,26 @@ function menuToggle() {
     } else {
         menuItems.style.opacity = '0';
     }
+}
+
+var productImg = document.getElementById('productImg');
+var smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function() {
+   productImg.src = smallImg[0].src; 
+}
+smallImg[1].onclick = function() {
+   productImg.src = smallImg[1].src; 
+}
+smallImg[2].onclick = function() {
+   productImg.src = smallImg[2].src; 
+}
+smallImg[3].onclick = function() {
+   productImg.src = smallImg[3].src; 
+}
+smallImg[4].onclick = function() {
+   productImg.src = smallImg[4].src; 
+}
+smallImg[5].onclick = function() {
+   productImg.src = smallImg[5].src; 
 }
